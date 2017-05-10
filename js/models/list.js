95adafc898083ae7e80c8765d140bf1cb13b87c8
Naz Khan <nazmuskhan@yahoo.com>
2017-05-10 00:03:26 -0400

    Done.

--- js/models/list.js
+++ js/models/list.js
@@ -1,2 +1,10 @@
 // List Model
-
+class List{
+  constructor(title){
+    this.title = title
+    this.id = List.all.length
+    this.tasks = []
+    List.all.push(this)
+  }
+}
+List.all = []
--- js/models/task.js
+++ js/models/task.js
@@ -1,2 +1,10 @@
 // Task Model
-
+class Task{
+  constructor(description, priority, list){
+    this.description = description
+    this.priority = priority
+    this.id = Task.counter
+    this.list = list
+  }
+}
+Task.counter = 0
